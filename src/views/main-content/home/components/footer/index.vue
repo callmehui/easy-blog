f2320be302f3bc2e5a48b4b64b1f19a4dcef9349
钟辉 <hui.zhong@wholion.com>
2021-11-25 10:50:41 +0800

    chore: 修改备案信息官网链接

--- src/views/main-content/home/components/footer/index.vue
+++ src/views/main-content/home/components/footer/index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="px-2 mt-4 text-xs text-gray-500">
-    <a class="block mb-1" href="http://www.beian.miit.gov.cn/" target="_blank">
+    <a class="block mb-1" href="http://beian.miit.gov.cn/" target="_blank">
       粤ICP备 19089643 号
     </a>
     <div class="flex flex-wrap items-center">
       <img class="mr-1" src="@/assets/image/gov.png" alt="政府图标" />
       <a
         class="link"
-        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030402003243"
+        href="http://beian.gov.cn/portal/registerSystemInfo?recordcode=44030402003243"
         target="_blank"
       >
         粤公网安备 44030402003243号
